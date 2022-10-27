73795ce213958f409fef8966bdf4c5556e09db3d
Kester Riley <kesterriley@hotmail.com>
2022-10-27 20:55:05 +0200

    ReadMe Updates

--- bin/fix-permissions.sh
+++ bin/fix-permissions.sh
@@ -2,6 +2,11 @@
 # Taken from https://raw.githubusercontent.com/openshift/sti-base/master/bin/fix-permissions
 # Fix permissions on the given directory to allow group read/write of 
 # regular files and execute of directories.
-chgrp -R 0 $1
-chmod -R g+rw $1
+#chgrp -R 0 $1
+#chmod -R g+rw $1
+#find $1 -type d -exec chmod g+x {} +
+chgrp -R 0 $1;
+find -L $1 -xtype l -exec chgrp 0 {} \;
+chmod -R g+rw $1;
+find -L $1 -xtype l -exec chmod g+rw {} \;
 find $1 -type d -exec chmod g+x {} +
